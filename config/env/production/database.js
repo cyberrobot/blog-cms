b1eb3157bfad853381d57b7ea160b0aaedf5c06d
Yaroslav Salmin <yaroslav.shoppertrak@gmail.com>
2023-03-30 12:58:07 +0100

    Fly.io deployment

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,20 +1,16 @@
-const { parse } = require("pg-connection-string");
-
-module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
-
-  return {
+const parse = require("pg-connection-string").parse;
+const config = parse(process.env.DATABASE_URL);
+module.exports = () => ({
+  connection: {
+    client: "postgres",
     connection: {
-      client: "postgres",
-      connection: {
-        host,
-        port,
-        database,
-        user,
-        password,
-        ssl: { rejectUnauthorized: false },
-      },
-      debug: false,
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: false,
     },
-  };
-};
+    debug: false,
+  },
+});
